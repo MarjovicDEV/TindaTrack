08cf5399ba3837d5bd3b72693806ea95a2ab3d0d
MarjovicDEV <marjovicalejado1232@gmail.com>
2026-04-30 23:36:47 +0800

    feat(Formatters): add functions for formatting long dates and Philippine date-time, including timezone adjustment

--- lib/core/utils/formatters.dart
+++ lib/core/utils/formatters.dart
@@ -9,3 +9,13 @@ final _currency = NumberFormat.currency(
 String formatCurrency(num value) => _currency.format(value);
 
 String formatDate(DateTime date) => DateFormat('MMM dd, yyyy').format(date);
+
+String formatLongDate(DateTime date) => DateFormat('MMMM d, yyyy').format(date);
+
+DateTime _toPhilippineTime(DateTime date) {
+  final utc = date.isUtc ? date : date.toUtc();
+  return utc.add(const Duration(hours: 8));
+}
+
+String formatPhilippineDateTime(DateTime date) =>
+    DateFormat('MMMM d, yyyy • hh:mm a').format(_toPhilippineTime(date));
