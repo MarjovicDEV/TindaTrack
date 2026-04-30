2c5311ebc6239bde5d6996175c59672214a83e37
MarjovicDEV <marjovicalejado1232@gmail.com>
2026-04-30 19:06:55 +0800

    feat(AppCopy): add AppCopy class for localized app strings in TindaTrack

---
+++ lib/core/resources/app_copy.dart
@@ -0,0 +1,24 @@
+class AppCopy {
+  static const appTitle = 'TindaTrack';
+
+  static const navDashboard = 'Ulat';
+  static const navInventory = 'Imbentaryo';
+  static const navSales = 'Benta';
+  static const navUtang = 'Utang';
+  static const navExpenses = 'Gastos';
+  static const navGrocery = 'Grocery';
+
+  static const summaryTitle = 'Buod ng Tindahan';
+  static const totalSales = 'Kabuuang Benta';
+  static const totalExpenses = 'Kabuuang Gastos';
+  static const netProfit = 'Netong Kita';
+  static const lowStockAlerts = 'Mababang Stock';
+
+  static const backupRestoreTitle = 'Backup at Restore';
+  static const exportJson = 'I-export JSON';
+  static const importJson = 'I-import JSON';
+  static const exportDb = 'I-export DB';
+  static const importDb = 'I-import DB';
+  static const replaceMode = 'Palitan lahat';
+  static const mergeMode = 'Pagsamahin';
+}
