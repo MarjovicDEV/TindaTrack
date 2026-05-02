72e154933b4fce7b7c6f601986e03e152570575e
MarjovicDEV <marjovicalejado1232@gmail.com>
2026-05-02 11:47:35 +0800

    feat(TindaTrackApp): integrate LocaleController and CurrencyController for localization and currency management, update MaterialApp to support multiple locales and localizations

--- lib/app/tinda_track_app.dart
+++ lib/app/tinda_track_app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import '../core/currency/currency_controller.dart';
+import '../core/locale/locale_controller.dart';
 import '../core/theme/app_theme.dart';
 import '../core/theme/theme_mode_controller.dart';
 import '../features/dashboard/presentation/home_shell_page.dart';
@@ -15,35 +18,54 @@ class TindaTrackApp extends StatefulWidget {
 
 class _TindaTrackAppState extends State<TindaTrackApp> {
   late final ThemeModeController _themeController;
-  late final bool _ownsController;
+  late final LocaleController _localeController;
+  late final CurrencyController _currencyController;
+  late final bool _ownsTheme;
 
   @override
   void initState() {
     super.initState();
-    _ownsController = widget.themeController == null;
+    _ownsTheme = widget.themeController == null;
     _themeController = widget.themeController ?? ThemeModeController();
+    _localeController = LocaleController();
+    _currencyController = CurrencyController();
     _themeController.load();
+    _localeController.load();
+    _currencyController.load();
   }
 
   @override
   void dispose() {
-    if (_ownsController) {
+    if (_ownsTheme) {
       _themeController.dispose();
     }
+    _localeController.dispose();
+    _currencyController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _themeController,
+      animation: Listenable.merge([_themeController, _localeController, _currencyController]),
       builder: (context, _) => MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'TindaTrack',
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
         themeMode: _themeController.themeMode,
-        home: HomeShellPage(themeController: _themeController),
+        locale: _localeController.locale,
+        supportedLocales: const [Locale('en'), Locale('fil')],
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        home: HomeShellPage(
+          themeController: _themeController,
+          localeController: _localeController,
+          currencyController: _currencyController,
+        ),
       ),
     );
   }
